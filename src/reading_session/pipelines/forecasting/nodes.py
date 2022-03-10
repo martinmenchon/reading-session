1c68fa735dd7ae751d5bbd5fc1c5882f7b6be636
Martin <martinmenchon@gmail.com>
2022-03-10 14:29:08 +0100

    test pre-commit

--- src/reading_session/pipelines/forecasting/nodes.py
+++ src/reading_session/pipelines/forecasting/nodes.py
@@ -1,14 +1,11 @@
-from sklearn.ensemble import RandomForestRegressor
 from skforecast.ForecasterAutoreg import ForecasterAutoreg
+from sklearn.ensemble import RandomForestRegressor
 
 
 def run_forecasting(data_train, steps, lags):
-    forecaster = ForecasterAutoreg(
-        regressor=RandomForestRegressor(random_state=123),
-        lags=lags
-    )
+    forecaster = ForecasterAutoreg(regressor=RandomForestRegressor(random_state=123), lags=lags)
 
-    forecaster.fit(y=data_train['y'])
+    forecaster.fit(y=data_train["y"])
     forecaster
     y_pred = forecaster.predict(steps=steps)
     return y_pred
--- src/reading_session/pipelines/results/nodes.py
+++ src/reading_session/pipelines/results/nodes.py
@@ -1,15 +1,13 @@
 import matplotlib.pyplot as plt
 from sklearn.metrics import mean_squared_error
 
-def results(data_train,y_true,y_pred):
+
+def results(data_train, y_true, y_pred):
     fig, ax = plt.subplots(figsize=(9, 4))
-    data_train['y'].plot(ax=ax, label='train')
-    y_true['y'].plot(ax=ax, label='test')
-    y_pred.plot(ax=ax, label='y_pred')
+    data_train["y"].plot(ax=ax, label="train")
+    y_true["y"].plot(ax=ax, label="test")
+    y_pred.plot(ax=ax, label="y_pred")
     ax.legend()
 
-    error_mse = mean_squared_error(
-        y_true=y_true['y'],
-        y_pred=y_pred
-    )
+    error_mse = mean_squared_error(y_true=y_true["y"], y_pred=y_pred)
     print(f"Error de test (mse): {error_mse}")
